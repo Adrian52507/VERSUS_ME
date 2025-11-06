54df2444d178b0825dfb1ccfb71489fd31c2aa31
Adrian <adrian52507@gmail.com>
2025-11-06 17:41:41 -0500

    Eliminación de api/index.js

--- versusme/backend/api/index.js
+++
@@ -1,7 +0,0 @@
-import app from "../src/server.js";
-
-// Vercel exige exportar una función handler, no un objeto o servidor iniciado.
-export default function handler(req, res) {
-  // Vercel usará esta función como el entrypoint.
-  return app(req, res);
-}
